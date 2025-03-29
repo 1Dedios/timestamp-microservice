35a1a1a3754b60ed79d110e807336da8f95c3527
Dedios <dediosprogramming@gmail.com>
2025-03-28 21:38:06 -0400

    Refactored: changed endpoint /api/:date to /date

--- server.js
+++ server.js
@@ -1,54 +1,39 @@
 const express = require("express");
 const app = express();
-const port = 3000;
-let absolutePath = __dirname + "/public/index.html";
-
+const PORT = 3000;
+let absolutePath = __dirname + "./public/index.html";
 app.use(express.static("public"));
 
 app.get("/", (req, res) => {
   res.sendFile(absolutePath);
 });
 
-app.get("/api/:date?", (req, res) => {
-  const dateParam = req.params.date;
-  const localDate = new Date(dateParam);
-  const unixTime = Math.floor(localDate.getTime() / 1000);
+app.get("/date", (req, res) => {
+  let currentTime = new Date();
+  let unixSeconds = Math.floor(currentTime.getTime() / 1000);
   const utcDate = new Date(
-    Date.UTC(localDate.getFullYear(), localDate.getMonth(), localDate.getDate())
+    Date.UTC(
+      currentTime.getFullYear(),
+      currentTime.getMonth(),
+      currentTime.getDate()
+    )
   );
   const utcDateAsString = utcDate.toUTCString();
-
-  if (Number.isNaN(dateParam)) {
-    res.json({ error: "Invalid Date" });
-  }
-
-  if (!dateParam) {
-    let currentTime = new Date();
-    let unixTime = Math.floor(currentTime.getTime() / 1000);
-    const utcDate = new Date(
-      Date.UTC(
-        currentTime.getFullYear(),
-        currentTime.getMonth(),
-        currentTime.getDate()
-      )
-    );
-    const utcDateAsString = utcDate.toUTCString();
-    res.json({ unix: unixTime, utc: utcDateAsString });
-  }
-  res.json({ unix: unixTime, utc: utcDateAsString });
+    
+  res.json({ unix: unixSeconds, utc: utcDateAsString });
 });
 
-// why doesn't the code below work?
-app.get("/api/:timestamp", (req, res) => {
-  // 1451001600000;
-  const param = req.params.timestamp;
-  const paramInMilliSeconds = parseInt(param, 10) * 1000;
-  const utcParamTime = new Date(paramInMilliSeconds);
+app.get("/timestamp-conversion/:timestamp", (req, res) => {
+  // TODO: accept timestamp in seconds and convert to human readable date UTC time zone (day of week, day of month, month, year, HH:mm:ss GMT)
+  const timeStampParam = req.params.timestamp;
+  const paramInMilliSeconds = parseInt(timeStampParam) * 1000;
+  const utcParamTime = new Date(param);
+  console.log('utc param time', utcParamTime)
   const utcParamTimeString = utcParamTime.toUTCString();
 
-  res.json({ unix: parseInt(param), utc: utcParamTimeString });
+  res.json({ unix: param, utc: utcParamTimeString });
 });
 
-app.listen(port, () => {
-  console.log(`Timestamp Microservice app listening on port ${port}`);
+app.listen(PORT, () => {
+  console.log(`Timestamp Microservice app listening on port ${PORT}`);
 });
